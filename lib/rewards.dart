ee0b0b1594518a29ea3d03b8366999d724d1f474
Blake Coman <minishbeard@gmail.com>
2019-09-15 22:04:56 +1000

    Rewards

--- lib/rewards.dart
+++ lib/rewards.dart
@@ -19,6 +19,8 @@ class Rewards extends StatefulWidget {
 }
 
 class RewardsState extends State<Rewards> {
+//  final bgColor = const Color(0xFFF5F5F5);
+//  final barColor = const Color(0xFF02735E);
 
   @override
   Widget build(BuildContext context) {
@@ -29,10 +31,78 @@ class RewardsState extends State<Rewards> {
           backgroundColor: barColor,
         ),
         body: Container(
-
-
-
+          child: _rewardList()
         )
+      );
+  }
+
+  _rewardList() {
+    return StreamBuilder(
+      stream: Firestore.instance.collection('events').document('event2').collection('Rewards').snapshots(),
+      builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+        if (!snapshot.hasData) return new Text("Now Loading...");
+        return ListView.builder(
+          itemCount: snapshot.data.documents.length,
+          itemBuilder: (context, index) {
+            DocumentSnapshot document = snapshot.data.documents[index];
+            return Card(
+              child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            ListTile(
+              title: new Text(document['name']),
+              //subtitle: new Text(document['description']),
+            ),
+            ButtonTheme.bar(
+              child: ButtonBar(
+                children: <Widget>[
+                  FlatButton(
+                    child: new Text('View Details'),
+                    //onPressed: () => _showDialog(document),
+                    onPressed: () {_showDialog(document);},
+                  ),
+                ],
+              ),
+            )
+          ],
+        )
+            );
+          },
+        );
+      },
     );
   }
+
+  void _showDialog(DocumentSnapshot document) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+        title: Text(document['name']),
+        content: Text("Placeholder Reward Informatioin"),
+        //content: Text("Description:\n" + document['description'] + "\n\nStart Date: " + document['startDate'] + "\n\nEnd Date: " + document['endDate'] + "\n\nStart Time: " + document['startTime'] + "\n\nEnd Time: " + document['endTime']),
+        actions: <Widget>[
+          Row(
+            children: <Widget>[
+              RaisedButton(
+                child: Text(
+                  "Go Back",
+                  style: TextStyle(
+                    color: bgColor,
+                  ),
+                ),
+                color: Colors.deepOrange,
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+              ),
+            ]
+          )
+        ]
+    );
+      }
+    );
+    
+  }
+
 }
